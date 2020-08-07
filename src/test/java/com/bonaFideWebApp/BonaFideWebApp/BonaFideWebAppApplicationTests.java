a6e2e34488dbdfc84a5e88bb9c391b3ecfc74a3b
lsebastian <v-lsebastian@egencia.com>
2020-08-07 22:14:12 +0530

    Initial file Uploads

---
+++ src/test/java/com/bonaFideWebApp/BonaFideWebApp/BonaFideWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonaFideWebApp.BonaFideWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonaFideWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
