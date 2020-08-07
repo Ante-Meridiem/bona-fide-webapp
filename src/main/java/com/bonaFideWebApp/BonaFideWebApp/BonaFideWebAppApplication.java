342c78780f03eb728ecef00976f78bd701d9135e
Lino SEBASTIAN <31162760+linosebastian@users.noreply.github.com>
2020-08-07 22:16:53 +0530

    Initial file Uploads (#1)
    Co-authored-by: lsebastian <v-lsebastian@egencia.com>

---
+++ src/main/java/com/bonaFideWebApp/BonaFideWebApp/BonaFideWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bonaFideWebApp.BonaFideWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonaFideWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonaFideWebAppApplication.class, args);
+	}
+
+}
